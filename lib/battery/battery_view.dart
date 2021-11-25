ba10520aafb194c5a7d07f5a9ab67ef7137ac258
Christof Kost <christof.kost@mway.io>
2021-11-25 18:17:30 +0100

    put font above battery

--- lib/battery/battery_view.dart
+++ lib/battery/battery_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 class BatteryView extends StatelessWidget {
@@ -6,13 +8,51 @@ class BatteryView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Icon(
-          Icons.battery_full,
-          size: 300,
-        )
-      ],
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Stack(
+            children: [
+              Transform.rotate(
+                angle: math.pi / 2,
+                child: Icon(
+                  Icons.battery_full,
+                  size: 300,
+                ),
+              ),
+              Positioned.fill(
+                child: Center(
+                    child: Text(
+                  "64%",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold),
+                )),
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {},
+                  elevation: 0,
+                  child: Icon(Icons.add),
+                ),
+                FloatingActionButton(
+                  onPressed: () {},
+                  elevation: 0,
+                  child: Icon(Icons.remove),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     ));
   }
 }
