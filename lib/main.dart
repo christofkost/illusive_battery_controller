89c18dc75b495261801af484e47633bdcabf5553
Christof Kost <christof.kost@mway.io>
2021-11-25 17:19:44 +0100

    updated linting

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
         title: 'Illusive Battery Controller', home: Text('initial'));
   }
 }
