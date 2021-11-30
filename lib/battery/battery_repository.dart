0386dcaee0f31fc9f9e2121b4d904ae6d1d71dc8
Christof Kost <christof.kost@mway.io>
2021-11-30 16:05:48 +0100

    added method channel

---
+++ lib/battery/battery_repository.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/services.dart';
+
+class BatteryRepository {
+  static const platform = MethodChannel('samples.flutter.dev/battery');
+
+  static Future<void> getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = '$result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level.";
+    }
+  }
+}
