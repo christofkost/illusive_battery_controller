216441d1cffc03da021d29058a233053a5aa7b89
Christof Kost <christof.kost@mway.io>
2021-12-01 15:51:57 +0100

    removed unnecessary center

--- lib/battery/battery_view.dart
+++ lib/battery/battery_view.dart
@@ -14,91 +14,89 @@ class BatteryView extends StatelessWidget {
       color: backgroundColor,
       duration: Duration(seconds: 1),
       curve: Curves.decelerate,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 50),
-              child: Text(
-                'Illusive Battery Controller',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 60,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(bottom: 50),
+            child: Text(
+              'Illusive Battery Controller',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 60,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
             ),
-            Stack(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 130,
-                      width: (MediaQuery.of(context).size.width) * 0.9 -
-                          _batteryBorderWidth,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(
-                              color: Colors.white, width: _batteryBorderWidth)),
-                    ),
-                    Container(
-                      height: 50,
-                      width: _batteryBorderWidth,
-                      color: Colors.white,
-                    )
-                  ],
-                ),
-                Positioned.fill(
-                  child: Center(
-                      child: Text(
-                    "64%",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 60,
-                        fontWeight: FontWeight.bold),
-                  )),
-                )
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ),
+          Stack(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  FloatingActionButton(
-                    backgroundColor: Colors.white,
-                    foregroundColor: backgroundColor,
-                    elevation: 2,
-                    onPressed: () {},
-                    child: Icon(Icons.remove),
+                  Container(
+                    height: 130,
+                    width: (MediaQuery.of(context).size.width) * 0.9 -
+                        _batteryBorderWidth,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        border: Border.all(
+                            color: Colors.white, width: _batteryBorderWidth)),
                   ),
-                  OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      side: BorderSide(
-                        width: 3.0,
-                        color: Colors.white,
-                      ),
-                      primary: Colors.white,
-                    ),
-                    child: Text('Back to reality'),
-                    onPressed: () {
-                      BatteryRepository.getBatteryLevel();
-                    },
-                  ),
-                  FloatingActionButton(
-                    backgroundColor: Colors.white,
-                    foregroundColor: backgroundColor,
-                    elevation: 2,
-                    onPressed: () {},
-                    child: Icon(Icons.add),
-                  ),
+                  Container(
+                    height: 50,
+                    width: _batteryBorderWidth,
+                    color: Colors.white,
+                  )
                 ],
               ),
-            )
-          ],
-        ),
+              Positioned.fill(
+                child: Center(
+                    child: Text(
+                  "64%",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold),
+                )),
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FloatingActionButton(
+                  backgroundColor: Colors.white,
+                  foregroundColor: backgroundColor,
+                  elevation: 2,
+                  onPressed: () {},
+                  child: Icon(Icons.remove),
+                ),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(
+                      width: 3.0,
+                      color: Colors.white,
+                    ),
+                    primary: Colors.white,
+                  ),
+                  child: Text('Back to reality'),
+                  onPressed: () {
+                    BatteryRepository.getBatteryLevel();
+                  },
+                ),
+                FloatingActionButton(
+                  backgroundColor: Colors.white,
+                  foregroundColor: backgroundColor,
+                  elevation: 2,
+                  onPressed: () {},
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     ));
   }
