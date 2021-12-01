3accfdeceaac56cd7357b590990709cc8b2ec2aa
Christof Kost <christof.kost@mway.io>
2021-12-01 13:54:09 +0100

    added read me

--- lib/battery/battery_view.dart
+++ lib/battery/battery_view.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'battery_repository.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:illusive_battery_controller/battery/battery_change_notifier.dart';
-import 'package:illusive_battery_controller/battery/battery_repository.dart';
 import 'package:illusive_battery_controller/battery/battery_view.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,20 +15,13 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return ChangeNotifierProvider(
-      create: (context) {
-        final batteryController = BatteryChangeNotifier(BatteryRepository());
-        batteryController.setOriginalBatteryData();
-        return batteryController;
-      },
-      child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            textTheme: GoogleFonts.montserratTextTheme(
-              Theme.of(context).textTheme,
-            ),
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          textTheme: GoogleFonts.montserratTextTheme(
+            Theme.of(context).textTheme,
           ),
-          home: BatteryView()),
-    );
+        ),
+        home: BatteryView());
   }
 }
